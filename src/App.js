c3610a91836b8d3fd2c16baf536e511812eb86b3
Aditya Jee <jeeadityajha1@gmail.com>
2020-05-20 04:25:52 +0530

    Top 10 Tracks Feature Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
+import { Provider } from './context';
+
 import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
-    </div>
+    <Provider>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={Index} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </Provider>
   );
 }
 
